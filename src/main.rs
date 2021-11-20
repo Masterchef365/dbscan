42023bb18740d8df37adba155700dc6cdb371043
Duncan Freeman <dfreeman@lucidyne.com>
2021-11-19 16:27:36 -0800

    Iterators

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{collections::HashMap, time::Instant};
 use rand::distributions::{Distribution, Uniform};
+use std::{collections::HashMap, time::Instant};
 
 type Point = [f32; 2];
 type IntPoint = [i32; 2];
@@ -7,12 +7,9 @@ type IntPoint = [i32; 2];
 fn random_points(n: usize, size: f32) -> Vec<Point> {
     let mut rng = rand::thread_rng();
     let dist = Uniform::new(-size, size);
-    (0..n).map(|_| 
-        [ 
-            dist.sample(&mut rng),
-            dist.sample(&mut rng)
-        ]
-    ).collect()
+    (0..n)
+        .map(|_| [dist.sample(&mut rng), dist.sample(&mut rng)])
+        .collect()
 }
 
 fn random_indices(n: usize, max: usize) -> Vec<usize> {
@@ -25,19 +22,19 @@ fn distance([x1, y1]: Point, [x2, y2]: Point) -> f32 {
     ((x1 - x2).powf(2.) + (y1 - y2).powf(2.)).sqrt()
 }
 
-fn naive_query(points: &[Point], queried_idx: usize, radius: f32) -> Vec<usize> {
+fn naive_query<'points>(
+    points: &'points [Point],
+    queried_idx: usize,
+    radius: f32,
+) -> impl Iterator<Item = usize> + 'points {
     let queried_point = points[queried_idx];
-    let mut indices = vec![];
-    for (point_idx, point) in points.iter().enumerate() {
-        if point_idx == queried_idx {
-            continue;
-        }
-
-        if distance(queried_point, *point) <= radius {
-            indices.push(point_idx);
-        }
-    }
-    indices
+    points
+        .iter()
+        .enumerate()
+        .filter_map(move |(point_idx, point)| {
+            (point_idx != queried_idx && distance(queried_point, *point) <= radius)
+                .then(|| point_idx)
+        })
 }
 
 struct QueryAccelerator {
@@ -54,19 +51,20 @@ impl QueryAccelerator {
                 .or_default()
                 .push(idx);
         }
-        
+
         //TODO: See if this speeds up queries do to cache locality!
         for indices in cells.values_mut() {
             indices.sort();
         }
 
-        Self {
-            cells,
-            radius,
-        }
+        Self { cells, radius }
     }
 
-    fn query_neighbors(&self, points: &[Point], queried_idx: usize) -> Vec<usize> {
+    fn query_neighbors<'s>(
+        &'s self,
+        points: &'s [Point],
+        queried_idx: usize,
+    ) -> impl Iterator<Item = usize> + 's {
         let query_point = points[queried_idx];
         let [origin_x, origin_y] = Self::calc_cell(query_point, self.radius);
 
@@ -82,22 +80,18 @@ impl QueryAccelerator {
             [1, 1],
         ];
 
-        let mut indices = vec![];
-
-        for [dx, dy] in NEIGHBOR_CELLS {
-            let key = [origin_x + dx, origin_y + dy];
-            if let Some(cell_indices) = self.cells.get(&key) {
-                for &idx in cell_indices {
-                    if idx != queried_idx {
-                        if distance(points[idx], query_point) <= self.radius {
-                            indices.push(idx);
-                        }
-                    }
-                }
-            }
-        }
-
-        indices
+        NEIGHBOR_CELLS
+            .into_iter()
+            .map(move |[dx, dy]| {
+                let key = [origin_x + dx, origin_y + dy];
+                self.cells.get(&key).map(|cell_indices| {
+                    cell_indices.iter().copied().filter(move |&idx| {
+                        idx != queried_idx && distance(points[idx], query_point) <= self.radius
+                    })
+                })
+            })
+            .flatten()
+            .flatten()
     }
 
     fn calc_cell([x, y]: Point, radius: f32) -> IntPoint {
@@ -108,23 +102,17 @@ impl QueryAccelerator {
 
 fn main() {
     let radius = 0.75;
-    let points = random_points(10_000, 1.);
-    let query_accel = QueryAccelerator::new(&points, radius);
+    let points = random_points(100_000, 1.);
 
-    let random_indices = random_indices(100_000, points.len());
+    let random_indices = random_indices(10_000, points.len());
     let start = Instant::now();
-    for &idx in &random_indices {
-        let indices = naive_query(&points, idx, radius);
-    }
-    println!("Naive took {}s", start.elapsed().as_secs_f32());
+    let sum = random_indices.iter().copied().map(|idx| naive_query(&points, idx, radius).sum::<usize>()).sum::<usize>();
+    println!("Naive took {}s, sum = {}", start.elapsed().as_secs_f32(), sum);
 
     let start = Instant::now();
     let accel = QueryAccelerator::new(&points, radius);
-    for &idx in &random_indices {
-        let indices = accel.query_neighbors(&points, idx);
-    }
-    println!("Accelerator took {}s", start.elapsed().as_secs_f32());
-
+    let sum = random_indices.iter().copied().map(|idx| accel.query_neighbors(&points, idx).sum::<usize>()).sum::<usize>();
+    println!("Accelerator  took {}s, sum = {}", start.elapsed().as_secs_f32(), sum);
 }
 
 #[cfg(test)]
@@ -144,8 +132,8 @@ mod tests {
         let query_accel = QueryAccelerator::new(&points, radius);
 
         for &test_idx in &random_indices(n_test_indices, points.len()) {
-            let mut naive = naive_query(&points, test_idx, radius);
-            let mut accel = query_accel.query_neighbors(&points, test_idx);
+            let mut naive: Vec<usize> = naive_query(&points, test_idx, radius).collect();
+            let mut accel: Vec<usize> = query_accel.query_neighbors(&points, test_idx).collect();
             naive.sort();
             accel.sort();
             assert_eq!(naive, accel);
